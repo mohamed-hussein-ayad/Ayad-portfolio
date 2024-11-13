5d79b265f977e75d61a7c18ba06c911842488462
mohamed ayad <mo.ayad.9@outlook.com>
2024-11-13 22:17:41 +0200

    add blog section is connected with database and the data is deliverd successfully

--- BACKEND/components/Blog.js
+++ BACKEND/components/Blog.js
@@ -1,29 +1,146 @@
+import { MdDeleteForever } from "react-icons/md";
 import ReactMarkdown from "react-markdown";
 import MarkdownEditor from "react-markdown-editor-lite";
 import "react-markdown-editor-lite/lib/index.css";
 import Spinner from "./Spinner";
-import { Children } from "react";
+import { useState } from "react";
+import { useRouter } from "next/router";
+import toast from "react-hot-toast";
+import axios from "axios";
+import { ReactSortable } from "react-sortablejs";
 
-export default function Blog() {
+export default function Blog({ _id }) {
+  const [redirect, setRedirect] = useState(false);
+
+  const router = useRouter();
+
+  const [title, setTitle] = useState("");
+  const [slug, setSlug] = useState("");
+  const [images, setImages] = useState([]);
+  const [description, setDescription] = useState("");
+  const [blogcategory, setBlogcategory] = useState([]);
+  const [tags, setTags] = useState([]);
+  const [status, setStatus] = useState("");
+
+  // for images uploading
+  const [isUploading, setIsUploading] = useState(false);
+  const uploadImagesQueue = [];
+  async function createBlog(ev) {
+    ev.preventDefault();
+
+    if (isUploading) {
+      await Promise.all(uploadImagesQueue);
+    }
+    const data = {
+      title,
+      slug,
+      images,
+      description,
+      blogcategory,
+      tags,
+      status,
+    };
+    if (_id) {
+      await axios.put("/api/blogs", { ...data, _id });
+      toast.success("Data Uploaded");
+    } else {
+      await axios.post("/api/blogs", data);
+      toast.success("Blog Created");
+    }
+    setRedirect(true);
+  }
+
+  async function uploadImages(ev) {
+    const files = ev.target?.files;
+    if (files?.length > 0) {
+      setIsUploading(true);
+      for (const file of files) {
+        const data = new FormData();
+        data.append("file", file);
+
+        // use the aixos.post method and push the promise to the queue
+
+        uploadImagesQueue.push(
+          axios.post("/api/upload", data).then((res) => {
+            setImages((oldImages) => [...oldImages, ...res.data.links]);
+          })
+        );
+      }
+      //wait for all images to finish uploading
+
+      await Promise.all(uploadImagesQueue);
+
+      setIsUploading(false);
+      toast.success("Images Uploaded");
+    } else {
+      toast.error("An error occurred!");
+    }
+  }
+
+  if (redirect) {
+    router.push("/blogs");
+    return null;
+  }
+
+  function updateImagesOrder(images) {
+    setImages(images);
+  }
+
+  function handleDeleteImage(index) {
+    const updatedImages = [...images];
+    updatedImages.splice(index, 1);
+    setImages(updatedImages);
+    toast.success("Image Deleted Successfully");
+  }
+
+  // for slug url
+
+  const handleSlugChange = (ev) => {
+    const inputValue = ev.target.value;
+    const newSlug = inputValue.replace(/\s+/g, "-"); // replace spaces with hyphens
+    setSlug(newSlug);
+  };
   return (
     <>
-      <form className="addWebsiteform">
+      <form className="addWebsiteform" onSubmit={createBlog}>
         {/* blog title */}
         <div className="w-100 flex flex-col flex-left mb-2">
           <label htmlFor="title">Title</label>
-          <input type="text" id="title" placeholder="Enter small title" />
+          <input
+            type="text"
+            id="title"
+            placeholder="Enter small title"
+            value={title}
+            onChange={(ev) => setTitle(ev.target.value)}
+          />
         </div>
         {/* blog slug url */}
         <div className="w-100 flex flex-col flex-left mb-2">
           <label htmlFor="slug">Slug (seo friendly url)</label>
-          <input type="text" id="slug" placeholder="Enter Slug url" />
+          <input
+            type="text"
+            id="slug"
+            placeholder="Enter Slug url"
+            value={slug}
+            onChange={handleSlugChange}
+          />
         </div>
         {/* blog category */}
         <div className="w-100 flex flex-col flex-left mb-2">
           <label htmlFor="category">
             Select Category (for multi select press ctr + left click)
           </label>
-          <select name="Category" id="Category" multiple>
+          <select
+            onChange={(e) =>
+              setBlogcategory(
+                Array.from(e.target.selectedOptions, (option) => option.value)
+              )
+            }
+            value={blogcategory}
+            name="Category"
+            id="Category"
+            multiple
+          >
             <option value="Node js">Node js</option>
             <option value="React js">React js</option>
             <option value="Next js">Next js</option>
@@ -42,14 +159,35 @@ export default function Blog() {
               className="mt-1"
               accept="image/*"
               multiple
+              onChange={uploadImages}
             />
           </div>
           <div className="w-100 flex flex-left mt-1">
-            <Spinner />
+            {isUploading && <Spinner />}
           </div>
         </div>
-        {/* image preview and image sortable */}
-        {/* pending */}
+        {/* image preview and image sortable with delete image */}
+        {!isUploading && (
+          <div className="flex">
+            <ReactSortable
+              list={Array.isArray(images) ? images : []}
+              setList={updateImagesOrder}
+              animation={200}
+              className="flex gap-1 "
+            >
+              {images?.map((link, index) => (
+                <div key={link} className="uploadedimg">
+                  <img src={link} alt="image" className="object-cover" />
+                  <div className="deleteimg">
+                    <button onClick={() => handleDeleteImage(index)}>
+                      <MdDeleteForever />
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </ReactSortable>
+          </div>
+        )}
         {/* markdown description */}
         <div className="description w-100 flex flex-col flex-left mb-2">
           <label htmlFor="description">
@@ -57,6 +195,8 @@ export default function Blog() {
             ![alt text][link])
           </label>
           <MarkdownEditor
+            value={description}
+            onChange={(ev) => setDescription(ev.text)}
             style={{ width: "100%", height: "400px" }}
             renderHTML={(text) => (
               <ReactMarkdown
@@ -108,7 +248,17 @@ export default function Blog() {
         {/* tags */}
         <div className="description w-100 flex flex-col flex-left mb-2">
           <label htmlFor="tags">Tags</label>
-          <select name="tags" id="tags" multiple>
+          <select
+            onChange={(e) =>
+              setTags(
+                Array.from(e.target.selectedOptions, (option) => option.value)
+              )
+            }
+            value={tags}
+            name="tags"
+            id="tags"
+            multiple
+          >
             <option value="html">html</option>
             <option value="css">css</option>
             <option value="javascript">javascript</option>
@@ -119,7 +269,12 @@ export default function Blog() {
         {/* blog status */}
         <div className="description w-100 flex flex-col flex-left mb-2">
           <label htmlFor="status">Status</label>
-          <select name="status" id="status">
+          <select
+            onChange={(ev) => setStatus(ev.target.value)}
+            value={status}
+            name="status"
+            id="status"
+          >
             <option value="">No select</option>
             <option value="draft">Draft</option>
             <option value="publish">Publish</option>
